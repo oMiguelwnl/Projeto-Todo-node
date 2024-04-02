51531c5ffafe549e1f555f2fb92ce61b5cb91cde
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-02 09:57:41 -0300

    Atualizar e deletar tarefas

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -19,6 +19,43 @@ export const create = async (req: Request, res: Response) => {
   res.json({ error: "Dados não enviados." });
 };
 
-export const update = async (req: Request, res: Response) => {};
+export const update = async (req: Request, res: Response) => {
+  let id: string = req.params.id;
 
-export const remove = async (req: Request, res: Response) => {};
+  let todo = await Todo.findByPk(id);
+
+  if (todo) {
+    if (req.body.title) {
+      todo.title = req.body.title;
+    }
+
+    if (req.body.done) {
+      switch (req.body.done.toLowerCase()) {
+        case "true":
+        case "1":
+          todo.done = true;
+        case "false":
+        case "0":
+          todo.done = false;
+          break;
+      }
+    }
+    await todo.save();
+
+    res.json({ item: todo });
+  } else {
+    res.status(404).json({ error: "Tarefa não encontrada." });
+  }
+};
+
+export const remove = async (req: Request, res: Response) => {
+  let id: string = req.params.id;
+
+  let todo = await Todo.findByPk(id);
+
+  if (todo) {
+    await todo.destroy();
+  }
+
+  res.json({});
+};
